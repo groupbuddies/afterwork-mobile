6a5cfe998607dce39efffa9cfbc3177abaf06635
Mário abreu <mjm.abreu@gmail.com>
2015-03-23 16:34:14 +0000

    Initial commit. Added a events tab, event detail and remove event action

---
+++ afterwork/www/js/controllers.js
@@ -0,0 +1,31 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  }
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('EventsCtrl', function($scope, Events) {
+  $scope.events = Events.all();
+  $scope.remove = function(event) {
+    Events.remove(event);
+  }
+})
+
+.controller('EventDetailCtrl', function($scope, $stateParams, Events) {
+  $scope.event = Events.get($stateParams.eventId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
