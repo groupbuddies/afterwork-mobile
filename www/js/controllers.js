233268913b53ffd4d23287d448e6a1278253dbf1
Gabriel Poça <gabrielpoca@gmail.com>
2015-03-25 10:32:50 +0000

    Add popup authentication saving to LS

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,3 +15,20 @@ angular.module('starter.controllers', [])
     $scope.cancelAttend = Events.cancelAttend;
     $scope.isAttending = Events.isAttending;
   });
+
+  .controller('SignInCtrl', function($scope, $window, localStorageService, $rootScope) {
+    $scope.signInWithTwitter = function() {
+      $window.addEventListener('message', function(e) {
+        if (e.origin === "http://localhost:3000") {
+          localStorageService.set('accessToken', e.data);
+          $rootScope.$emit('access_token');
+        }
+      }, false);
+
+      $window.open(
+        "http://localhost:3000/users/auth/twitter?type=cordova",
+        "_blank",
+        "height=400, width=550, status=yes, toolbar=no, menubar=no, location=no,addressbar=no"
+      );
+    };
+  });
