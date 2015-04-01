02977fbdcffbe6eadbe140f154ef5267ed3937f7
Mário abreu <mjm.abreu@gmail.com>
2015-04-01 10:52:45 +0100

    put the add/remove availability buttons in a custom directive

--- www/js/directives.js
+++ www/js/directives.js
@@ -2,6 +2,18 @@ angular.module('starter.directives', [])
 
 .directive('awEventForm', function (){
   return {
-    templateUrl: 'templates/event-form.html'
+    templateUrl: 'templates/partials/event-form.html'
+  }
+})
+
+.directive('awAddAvailability', function (){
+  return {
+    templateUrl: 'templates/partials/add-availability.html'
+  }
+})
+
+.directive('awRemoveAvailability', function (){
+  return {
+    templateUrl: 'templates/partials/remove-availability.html'
   }
 })
