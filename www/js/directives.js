142c6d97a8fd48d9772ad9c8294d0bba838453a6
Mário abreu <mjm.abreu@gmail.com>
2015-04-01 15:25:42 +0100

    Improved the create button directive.

--- www/js/directives.js
+++ www/js/directives.js
@@ -17,3 +17,40 @@ angular.module('starter.directives', [])
     templateUrl: 'templates/partials/remove-availability.html'
   }
 })
+
+.directive('awAttending', function () {
+  return {
+    templateUrl: 'templates/partials/user-attending.html'
+  }
+})
+
+.directive('awCreateEventButton', function() {
+  return {
+    scope: {
+      create: '&'
+    },
+    templateUrl: "templates/partials/create-event-button.html",
+    link : function(scope, element, attrs) {
+      scope.labelButton = attrs['labelButton'];
+      scope.icon = attrs['icon'];
+    }
+  }
+})
+
+.directive('awAvailabilityForm', function() {
+  return {
+    templateUrl: 'templates/partials/availability-form.html'
+  }
+})
+
+.directive('awEventDetail', function() {
+  return {
+    templateUrl: 'templates/partials/event-detail-info.html'
+  }
+})
+
+.directive('awAvailabilityDetail', function() {
+  return {
+    templateUrl: 'templates/partials/availability-detail.html'
+  }
+})
